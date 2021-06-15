e6ebbcfdeab19efc71c2ee7f88fd39c2e6863d3f
AndrewSkv <skvoznikovav@yandex.ru>
2021-06-15 16:53:03 +0400

    Initial commit

---
+++ app/src/main/java/com/example/listfragment/DetailFragment.java
@@ -0,0 +1,27 @@
+package com.example.listfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+
+public class DetailFragment extends Fragment {
+
+//    public DetailFragment(){
+//        super(R.layout.fragment_detail);
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    // обновление текстового поля
+    public void setSelectedItem(String selectedItem) {
+        TextView view = (TextView) getView().findViewById(R.id.detailsText);
+        view.setText(selectedItem);
+    }
+}
